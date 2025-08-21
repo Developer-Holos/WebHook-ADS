df9dc5e360d8fba9e09ac1dc46e9f1f441a58118
Diego Larios <d.larios@rcs.com.pe>
2025-08-21 09:55:22 -0500

    fix server

--- db.js
+++ db.js
@@ -3,8 +3,21 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: "postgresql://postgres:JONdBgamupvtqnAePuaaBoJWOXCiRmNn@shortline.proxy.rlwy.net:32303/railway",
   ssl: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
   },
 });
+
+(async () => {
+  try {
+    // 🔹 Ajusta automáticamente la secuencia de IDs en la tabla leads
+    await pool.query(`
+      SELECT setval('leads_id_seq', COALESCE((SELECT MAX(id) FROM leads), 1), true);
+    `);
+
+    console.log("✅ Secuencia de leads_id_seq actualizada correctamente");
+  } catch (error) {
+    console.error("❌ Error actualizando la secuencia:", error);
+  }
+})();
 
 module.exports = pool;
